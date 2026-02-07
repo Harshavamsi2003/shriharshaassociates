bf167ea91a1da11f29efa082b0d791de31db9e22
HarshavamsiPrakash <harshavamsiprakash566@gmail.com>
2026-02-08 00:07:37 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,31 @@
 
 
+// import React from "react";
+// import ReactDOM from "react-dom/client";
+// import App from "./App";
+
+// /* GLOBAL STYLES */
+// import './styles/global-mobile-fix.css';
+// import "./styles/base.css";
+// import "./styles/header.css";
+// import "./styles/hero.css";
+// import "./styles/stats.css";
+// import "./styles/about-page.css";
+// import "./styles/services.css";
+// import "./styles/projects.css";
+// import "./styles/testimonials.css";
+// import "./styles/contact.css";
+// import "./styles/footer.css";
+// import "./styles/floating-ui.css";
+// import './styles/mobile-fix.css'; 
+
+// ReactDOM.createRoot(document.getElementById("root")).render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+
+
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
@@ -13,6 +39,7 @@ import "./styles/stats.css";
 import "./styles/about-page.css";
 import "./styles/services.css";
 import "./styles/projects.css";
+import "./styles/residuals-page.css";
 import "./styles/testimonials.css";
 import "./styles/contact.css";
 import "./styles/footer.css";
